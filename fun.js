e2850c0495e495e2183df08102b046b94e3163a6
PHD-Cod <pedro_doratiotto@hotmail.com>
2024-12-17 17:24:57 -0300

    terceiro commit

---
+++ fun.js
@@ -0,0 +1,19 @@
+torrar("com pão de forma")
+torrar("com pão francês")
+
+function torrar(pao) {
+    console.log("fiz torrada com " + pao)
+}
+
+
+//2
+
+function soma(numA, numB){
+    let somatorio = numA + numB
+    return somatorio
+}
+
+let resultado = soma (5, 10)
+
+console.log("sendo assim o valor de resultado é " + resultado)
+
