0ca07c0a896eaef2b79e8a09efd0b55a16eb4c9b
PHD-Cod <pedro_doratiotto@hotmail.com>
2025-01-07 14:59:44 -0300

    Único commit

--- class.js
+++ class.js
@@ -3,10 +3,30 @@ class heroi {
         this.nome = nome
         this.idade = idade
         this.tipo = tipo
+        this.arma = this.determinarArma()
+    }
+
+    determinarArma() {
+        switch (this.tipo) {
+            case "guerreiro":
+                return "espada";
+            case "mago":
+                return "magia";
+            case "monge":
+                return "artes marciais";
+            case "ninja":
+                return "shuriken";
+            default:
+                return "desconhecida";
     }
 }
 
+escrever (){
+    console.log(`O ${this.tipo} atacou usando ${this.arma}`)
+    }
+}
+
+
 let gameHeroi = new heroi ("Karatos", 29, "guerreiro")
 
-console.log (gameHeroi.idade)
-
+gameHeroi.escrever()
